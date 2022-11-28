01185d6ad8164964d2f0422e62011d51c288eb29
yu-heejin <jkgt0521@naver.com>
2022-11-28 22:34:01 +0900

    feat: #13 Create Post Entity

--- src/main/java/com/techeeresc/tab/TabApplication.java
+++ src/main/java/com/techeeresc/tab/TabApplication.java
@@ -2,11 +2,11 @@ package com.techeeresc.tab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TabApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TabApplication.class, args);
 	}
--- src/main/java/com/techeeresc/tab/domain/post/entity/Post.java
+++ src/main/java/com/techeeresc/tab/domain/post/entity/Post.java
@@ -1,4 +1,32 @@
 package com.techeeresc.tab.domain.post.entity;
 
-public class Post {
+import com.techeeresc.tab.global.common.Timestamp;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "post")
+public class Post extends Timestamp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "member_id", nullable = false)
+    private Long memberId;       // 외래키, 향후 수정할 예정
+    @Column(name = "category", nullable = false)
+    private String category;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "content", nullable = false)
+    private String content;
+    @Column(name = "file")
+    private String file;
+    @Column(name = "image")
+    private String image;
+    @Column(name = "hashtags")
+    private String hashtags;    // 배열 형태로 변경
+    @Column(name = "is_anonymous", nullable = false)
+    private boolean isAnonymous;
+    @Column(name = "like_number", nullable = false)
+    private int likeNumber;
+    @Column(name = "views", nullable = false)
+    private int views;
 }
