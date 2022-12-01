d2f99c9d008bd152d61dd37d313f6bedbd4561f7
yu-heejin <jkgt0521@naver.com>
2022-12-01 18:11:58 +0900

    fix: #13 id 컬럼 타입 Unsigned int로 수정

---
+++ src/main/java/com/techeeresc/tab/domain/post/dto/PostDto.java
@@ -0,0 +1,24 @@
+package com.techeeresc.tab.domain.post.dto;
+
+public class PostDto {
+
+    private Long memberId;       // TODO: 외래키, 향후 외래키 매핑 필요
+
+    private String category;
+
+    private String title;
+
+    private String content;
+
+    private String file;
+
+    private String image;
+
+    private String hashtags;    // TODO: 한번에 여러개의 값을 받을 수 있도록 변경해야한다.
+
+    private boolean isAnonymous;
+
+    private int likeNumber;
+
+    private int views;
+}
--- src/main/java/com/techeeresc/tab/domain/post/entity/Post.java
+++ src/main/java/com/techeeresc/tab/domain/post/entity/Post.java
@@ -1,32 +1,47 @@
 package com.techeeresc.tab.domain.post.entity;
 
 import com.techeeresc.tab.global.common.Timestamp;
-import javax.persistence.*;
+import lombok.Getter;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
 @Entity
 @Table(name = "post")
 public class Post extends Timestamp {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)   //MySQL의 자동 생성 방식
+    @Column(columnDefinition = "INT UNSIGNED")
     private Long id;
+
     @Column(name = "member_id", nullable = false)
-    private Long memberId;       // 외래키, 향후 수정할 예정
+    private Long memberId;       // TODO: 외래키, 향후 외래키 매핑 필요
+
     @Column(name = "category", nullable = false)
     private String category;
+
     @Column(name = "title", nullable = false)
     private String title;
+
     @Column(name = "content", nullable = false)
     private String content;
+
     @Column(name = "file")
     private String file;
+
     @Column(name = "image")
     private String image;
+
     @Column(name = "hashtags")
-    private String hashtags;    // 배열 형태로 변경
+    private String hashtags;    // TODO: 한번에 여러개의 값을 받을 수 있도록 변경해야한다.
+
     @Column(name = "is_anonymous", nullable = false)
     private boolean isAnonymous;
+
     @Column(name = "like_number", nullable = false)
     private int likeNumber;
+
     @Column(name = "views", nullable = false)
     private int views;
 }
