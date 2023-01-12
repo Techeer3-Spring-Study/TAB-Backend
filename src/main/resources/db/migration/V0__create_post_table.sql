8b54055b3e7f24eb05b6fbe9440a41a7b6e392bf
yu-heejin <jkgt0521@naver.com>
2023-01-12 16:35:59 +0900

    fix: [#28] test 코드 관련 오류 수정

--- src/main/resources/db/migration/V0__create_post_table.sql
+++ src/main/resources/db/migration/V0__create_post_table.sql
@@ -16,3 +16,5 @@ create table post_test (
       views integer default 0 not null,
       primary key (id)
 );
+
+INSERT INTO post_test (member_id, category, title, content, file, image, hashtags, created_at, updated_at) VALUES (1, '개발', '페이징 처리 테스트하기', '안녕하세요!', 'www.file.com', 'www.image.com', '#hashtags', NOW(), NOW());
