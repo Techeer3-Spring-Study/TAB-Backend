0a51e210748f10da14b376ed83968aeffb324565
yu-heejin <jkgt0521@naver.com>
2023-01-16 21:55:45 +0900

    feat: [#29] 테스트 코드용 데이터베이스 연결하기

--- src/test/java/com/techeeresc/tab/domain/post/service/PostServiceTest.java
+++ src/test/java/com/techeeresc/tab/domain/post/service/PostServiceTest.java
@@ -1,26 +1,51 @@
 package com.techeeresc.tab.domain.post.service;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.techeeresc.tab.domain.post.dto.mapper.PostMapper;
 import com.techeeresc.tab.domain.post.dto.request.PostCreateRequestDto;
 import com.techeeresc.tab.domain.post.entity.Post;
+import com.techeeresc.tab.domain.post.entity.QPost;
+import com.techeeresc.tab.domain.post.repository.PostQueryDslRepository;
 import com.techeeresc.tab.domain.post.repository.PostRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
-@ExtendWith(MockitoExtension.class)   // 이거 넣으니까 되는 이유는..?
+//@ExtendWith(MockitoExtension.class)   // 이거 넣으니까 되는 이유는..?
+//@AutoConfigureMockMvc
+@SpringBootTest
 class PostServiceTest {
-    @Mock
+    //@Mock
+    @Autowired
     private PostRepository postRepository;
-    @Mock
+   //@Mock
+    @Autowired
     private PostMapper postMapper;
+    //@Mock
+    @Autowired
+    private JPAQueryFactory jpaQueryFactory;
+    //@Mock
+    @Autowired
+    private PostQueryDslRepository postQueryDslRepository;
+    //@InjectMocks
+    @Autowired
+    private PostService postService;
 
     @Test
     @Transactional
@@ -36,9 +61,14 @@ class PostServiceTest {
                         .content("재밌네요")
                         .hashtags("#개발")
                         .build();
-        // when
-        Post result = postRepository.save(postMapper.saveDataToEntity(postCreateRequestDto));
-        // then
-        Assertions.assertEquals(2, result.getId());
+
+        Post result = new Post();
+//        given(postRepository.save(postMapper.saveDataToEntity(postCreateRequestDto)))
+//                .willReturn(result);
+
+        Post result2 = postService.insertPost(postCreateRequestDto);
+
+        // System.out.println(result.getId());
+        System.out.println(result2.getTitle());
     }
 }
--- src/test/resources/db/migration/V1__create_table.sql
+++ src/test/resources/db/migration/V1__create_table.sql
@@ -1,10 +1,10 @@
 create table bookmark (
-  id INT UNSIGNED not null auto_increment,
-  member_id INT UNSIGNED not null,
-  post_id INT UNSIGNED not null,
-  created_at datetime(6),
-  updated_at datetime(6),
-  primary key (id)
+    id INT UNSIGNED not null auto_increment,
+    member_id INT UNSIGNED not null,
+    post_id INT UNSIGNED not null,
+    created_at datetime(6),
+    updated_at datetime(6),
+    primary key (id)
 );
 
 create table share_info (
@@ -20,18 +20,18 @@ create table share_info (
 );
 
 create table post (
-      id INT UNSIGNED not null auto_increment,
-      created_at datetime(6),
-      updated_at datetime(6),
-      category varchar(255) not null,
-      content varchar(255) not null,
-      file varchar(255),
-      hashtags varchar(255),
-      image varchar(255),
-      is_anonymous bit default false not null,
-      like_numbers integer default 0 not null,
-      member_id bigint not null,
-      title varchar(255) not null,
-      views integer default 0 not null,
-      primary key (id)
+    id INT UNSIGNED not null auto_increment,
+    created_at datetime(6),
+    updated_at datetime(6),
+    category varchar(255) not null,
+    content varchar(255) not null,
+    file varchar(255),
+    hashtags varchar(255),
+    image varchar(255),
+    is_anonymous bit default false not null,
+    like_numbers integer default 0 not null,
+    member_id bigint not null,
+    title varchar(255) not null,
+    views integer default 0 not null,
+    primary key (id)
 );
