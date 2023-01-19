d77383cc345c7c7ac645d992b827c44b624a5b8a
yu-heejin <jkgt0521@naver.com>
2023-01-19 18:43:55 +0900

    feat: [#44] Swagger 연동하기

--- src/main/java/com/techeeresc/tab/global/config/SwaggerConfig.java
+++ src/main/java/com/techeeresc/tab/global/config/SwaggerConfig.java
@@ -1,4 +1,25 @@
 package com.techeeresc.tab.global.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+
 public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("tab-api-v1")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI tabOpenApi() {
+        return new OpenAPI()
+                .info(new Info().title("TAB API")
+                        .description("Techeer 3rd Spring Boot Project API")
+                        .version("v0.0.1")
+                );
+    }
 }
