e57790465efa288c6c1be356368f4158aceb1803
zyoon <develop_z@naver.com>
2023-01-20 00:01:20 +0900

    fix: [#37] 404error 해결

--- src/main/java/com/techeeresc/tab/domain/member/controller/MemberController.java
+++ src/main/java/com/techeeresc/tab/domain/member/controller/MemberController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/member")
 public class MemberController {
