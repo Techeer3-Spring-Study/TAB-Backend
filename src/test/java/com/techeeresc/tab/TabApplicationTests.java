cbef16e1ca487acf575439dc035287a3b82d93a5
yu-heejin <jkgt0521@naver.com>
2023-01-19 21:59:35 +0900

    feat: [#29] Mockito를 사용한 서비스 계층 테스트 코드 작성

--- src/test/java/com/techeeresc/tab/TabApplicationTests.java
+++ src/test/java/com/techeeresc/tab/TabApplicationTests.java
@@ -3,7 +3,7 @@ package com.techeeresc.tab;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
+@SpringBootTest
 // 어노테이션은 스프링을 실제 띄워서 컨테이너를 띄우거나 DI를 하여 스프링과 동일한 환경에서 테스트할 수 있고,
 // 단순 자바 코드로서 테스트가 아닌 DB연동 등 통합 테스트에 자주 사용한다.
 // 하지만 김영한님의 말을 따르면
--- src/test/java/com/techeeresc/tab/domain/post/service/PostServiceTest.java
+++ src/test/java/com/techeeresc/tab/domain/post/service/PostServiceTest.java
@@ -1,26 +1,40 @@
 package com.techeeresc.tab.domain.post.service;
 
-import com.techeeresc.tab.domain.post.dto.request.PageRequest;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.techeeresc.tab.domain.post.dto.mapper.PostMapper;
 import com.techeeresc.tab.domain.post.dto.request.PostCreateRequestDto;
 import com.techeeresc.tab.domain.post.entity.Post;
+import com.techeeresc.tab.domain.post.repository.PostRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.test.annotation.Rollback;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 import javax.transaction.Transactional;
-import java.util.List;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class PostServiceTest {
-    @Autowired
+    @Mock   // 가짜 객체, 테스트 Run 시 실제 객체가 아닌 Mock 객체 반환
+    private PostRepository postRepository;
+    @Mock
+    private JPAQueryFactory jpaQueryFactory;
+    @Spy   // 실제 인스턴스를 사용한다.
+    private PostMapper postMapper;
+    @InjectMocks   // 테스트 대상 어노테이션, Mock 객체가 주입될 클래스
     private PostService postService;
+    private PostCreateRequestDto postCreateRequestDto;
+    private Post post;
 
-    @Test
-    @Transactional
-    @Rollback(false)
-    void 게시물_생성_Mockito() {
-        PostCreateRequestDto postCreateRequestDto =
+    @BeforeEach
+    void setUp() {
+        postCreateRequestDto =
                 PostCreateRequestDto.builder()
                         .memberId(1L)
                         .title("JunitTest")
@@ -31,22 +45,62 @@ class PostServiceTest {
                         .hashtags("#개발")
                         .build();
 
+        post = postMapper.saveDataToEntity(postCreateRequestDto);
+    }
+
+    @Test
+    @DisplayName("게시물 생성")
+    @Transactional
+    void 게시물_생성() throws Exception {
+        // given
+        when(postRepository.save(post)).thenReturn(post);
+
+        // when
         Post result = postService.insertPost(postCreateRequestDto);
 
-        System.out.println(result.getTitle());
-        System.out.println(result.getId());
+        // then
+        verify(postRepository).save(result);
     }
 
     @Test
     @Transactional
-    void 불러오기() {
-        PageRequest pageRequest = new PageRequest();  // page 1, size 10
-        Pageable pageable = pageRequest.of();
+    void 페이징_결과_불러오기() {
+        // given
+        when(postRepository.findById(1L)).thenReturn()
 
-        List<Post> pages = postService.findAllPostListWithQueryDsl(pageable);
+        // when
 
-        for (Post post : pages) {
-            System.out.println(post.getId());
-        }
+        // then
     }
+//
+//    @Test
+//    @Transactional
+//    // @Rollback(false)
+//    void 게시물_삭제() {
+//
+//    }
+//
+//    @Test
+//    @Transactional
+//    void 게시물_수정() {
+//
+//    }
+//
+//    @Test
+//    @Transactional
+//    void 좋아요_증가() {
+//
+//    }
+//
+//    @Test
+//    @Transactional
+//    void 제목으로_검색하기() {
+//
+//    }
+//
+//    @Test
+//    @Transactional
+//    void 게시물_없을때_예외처리() {
+//
+//    }
 }
