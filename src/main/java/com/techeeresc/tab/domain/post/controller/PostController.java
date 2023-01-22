4c76e4f01f9420be4bcf2b911b3255146983055a
yu-heejin <jkgt0521@naver.com>
2023-01-23 02:18:51 +0900

    feat: [#44] swagger response 설정하기

--- src/main/java/com/techeeresc/tab/domain/post/controller/PostController.java
+++ src/main/java/com/techeeresc/tab/domain/post/controller/PostController.java
@@ -16,6 +16,7 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageImpl;
@@ -26,6 +27,22 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@ApiResponses(
+    {
+        @ApiResponse(responseCode = "400", description = "BAD REQUEST by Parameter Missing",
+                content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+        ),
+        @ApiResponse(responseCode = "400", description = "BAD REQUEST by Type Mismatch",
+                content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+        ),
+        @ApiResponse(responseCode = "404", description = "NOT FOUND",
+                content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+        ),
+        @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR",
+                content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+        ),
+    }
+)
 @Tag(name = "post", description = "Post API")
 @RestController
 @RequiredArgsConstructor
@@ -36,19 +53,7 @@ public class PostController {
 
     @Operation(summary = "create post", description = "Method: POST, success response code: 201")
     @ApiResponse(responseCode = "201", description = "CREATED",
-            content = @Content(schema = @Schema(implementation = PostResponseDto.class))
-    )
-    @ApiResponse(responseCode = "400", description = "BAD REQUEST by Parameter Missing",
-            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
-    )
-    @ApiResponse(responseCode = "400", description = "BAD REQUEST by Type Mismatch",
-            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
-    )
-    @ApiResponse(responseCode = "404", description = "NOT FOUND",
-            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
-    )
-    @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR",
-            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+        content = @Content(schema = @Schema(implementation = PostResponseDto.class))
     )
     @PostMapping
     public ResponseEntity<PostResponseDto> createPost(@RequestBody @Valid PostCreateRequestDto postCreateRequestDto) {
@@ -57,6 +62,9 @@ public class PostController {
     }
 
     @Operation(summary = "find all post by paging", description = "Method: GET, success response code: 200, parameter: ?page=&size=")
+    @ApiResponse(responseCode = "200", description = "OK",
+        content = @Content(schema = @Schema(implementation = PostResponseDto.class))
+    )
     @GetMapping
     public ResponseEntity<List<Post>> findAllPosts(PageRequest pageRequest) {
         // return new ResponseEntity<>(POST_SERVICE.findAllPost(), HttpStatus.OK);
@@ -66,13 +74,17 @@ public class PostController {
     }
 
     @Operation(summary = "update post", description = "Method: PUT, success response code: 201")
+    @ApiResponse(responseCode = "201", description = "CREATED",
+        content = @Content(schema = @Schema(implementation = PostResponseDto.class))
+    )
     @PutMapping
-    public ResponseEntity<PostResponseDto> updatePost(@RequestBody PostUpdateRequestDto postUpdateRequestDto) {
+    public ResponseEntity<PostResponseDto> updatePost(@RequestBody @Valid PostUpdateRequestDto postUpdateRequestDto) {
         Post updatePostResult = POST_SERVICE.updatePost(postUpdateRequestDto);
         return new ResponseEntity<>(POST_MAPPER.getDataFromEntity(updatePostResult), HttpStatus.CREATED);
     }
 
     @Operation(summary = "delete post", description = "Method: DELETE, success response code: 200, 처리 완료 후 메인 화면 이동 필요")
+    @ApiResponse(responseCode = "200", description = "OK")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePost(@Parameter(description = "페이지 아이디", in = ParameterIn.PATH) @PathVariable Long id) {
         POST_SERVICE.deletePost(id);
@@ -80,6 +92,7 @@ public class PostController {
     }
 
     @Operation(summary = "view one post", description = "Method: GET, success response code: 200")
+    @ApiResponse(responseCode = "200", description = "OK")
     @GetMapping("/{id}")
     public ResponseEntity<PostResponseDto> findPostAndIncreaseViews(@Parameter(description = "페이지 아이디", in = ParameterIn.PATH) @PathVariable Long id) {
         Post findPostResult = POST_SERVICE.findPostByIdAndIncreaseViews(id);
@@ -87,6 +100,7 @@ public class PostController {
     }
 
     @Operation(summary = "increase like number", description = "Method: POST, success response code: 201")
+    @ApiResponse(responseCode = "201", description = "CREATED")
     @PostMapping("/{id}")
     public ResponseEntity<PostResponseDto> increaseLikeNumbers(@Parameter(description = "페이지 아이디", in = ParameterIn.PATH) @PathVariable Long id) {
         Post clickLikePost = POST_SERVICE.increaseLikeNumbers(id);
@@ -94,6 +108,7 @@ public class PostController {
     }
 
     @Operation(summary = "search post by title", description = "Method: GET, success response code: 200")
+    @ApiResponse(responseCode = "200", description = "OK")
     @GetMapping("/search/{word:.+}")   /* PathVariable에 특수문자 허용 */
     public ResponseEntity<List<Post>> findPostSearchResults(@Parameter(description = "게시물 제목", in = ParameterIn.PATH) @PathVariable String word) {
         List<Post> postSearchResults = POST_SERVICE.findByTitleContainsWordWithQueryDsl(word);
