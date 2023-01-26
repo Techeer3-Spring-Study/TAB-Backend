c99f89bd1e27c266c821139c631acce4d95eff48
yu-heejin <jkgt0521@naver.com>
2023-01-26 22:24:10 +0900

    feat: [#29] 서비스 테스트 코드 - 게시물 조회

--- src/test/java/com/techeeresc/tab/domain/post/service/PostServiceTest.java
+++ src/test/java/com/techeeresc/tab/domain/post/service/PostServiceTest.java
@@ -2,8 +2,10 @@ package com.techeeresc.tab.domain.post.service;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.techeeresc.tab.domain.post.dto.mapper.PostMapper;
+import com.techeeresc.tab.domain.post.dto.request.PageRequest;
 import com.techeeresc.tab.domain.post.dto.request.PostCreateRequestDto;
 import com.techeeresc.tab.domain.post.entity.Post;
+import com.techeeresc.tab.domain.post.entity.QPost;
 import com.techeeresc.tab.domain.post.repository.PostRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,14 +14,18 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
+
 import javax.transaction.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
 
 @ExtendWith(MockitoExtension.class)
 class PostServiceTest {
@@ -69,7 +75,44 @@ class PostServiceTest {
         Post result = postService.insertPost(postCreateRequestDto);
         // then
         verify(postRepository).save(any());
-        assertThat(result.getContent()).isEqualTo(post.getContent());
+        assertThat(result).isEqualTo(post);
+    }
+
+    @Test
+    @Transactional
+    void 모든_게시물_조회() throws Exception {
+        // given
+        QPost qPost = QPost.post;
+        PageRequest pageRequest = new PageRequest();
+        Pageable pageable = pageRequest.of();
+
+        List<Post> posts = Arrays.asList(
+                Post.builder()
+                        .memberId(1L)
+                        .title("게시물 테스트 코드 테스트")
+                        .image("www.image.com")
+                        .file("file.com")
+                        .category("개발")
+                        .content("재밌네요")
+                        .hashtags("#개발")
+                        .build()
+        );
+
+        when(jpaQueryFactory.selectFrom(qPost)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch()).thenReturn(posts);
+
+        // when
+        List<Post> results = postService.findAllPostListWithQueryDsl(pageable);
+
+        // then
+        verify(jpaQueryFactory).selectFrom(qPost)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        assertThat(results).isEqualTo(posts);
     }
 //
 //    @Test
