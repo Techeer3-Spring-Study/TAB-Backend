fc3447d92363f8e5c2437b6c64a80b00c0421ef2
zyoon <develop_z@naver.com>
2023-02-06 02:52:09 +0900

    feat:[#59] swagger controller 추가

--- src/main/java/com/techeeresc/tab/domain/comment/controller/CommentController.java
+++ src/main/java/com/techeeresc/tab/domain/comment/controller/CommentController.java
@@ -6,12 +6,36 @@ import com.techeeresc.tab.domain.comment.dto.request.CommentUpdateRequestDto;
 import com.techeeresc.tab.domain.comment.dto.response.CommentResponseDto;
 import com.techeeresc.tab.domain.comment.entity.Comment;
 import com.techeeresc.tab.domain.comment.service.CommentService;
+import com.techeeresc.tab.global.exception.response.ErrorResponse;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.*;
+import io.swagger.v3.oas.annotations.responses.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
+@ApiResponses(
+        {
+                @ApiResponse(responseCode = "400", description = "BAD REQUEST by Parameter Missing",
+                        content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                ),
+                @ApiResponse(responseCode = "400", description = "BAD REQUEST by Type Mismatch",
+                        content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                ),
+                @ApiResponse(responseCode = "404", description = "NOT FOUND",
+                        content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                ),
+                @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR",
+                        content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                ),
+        }
+)
+@Tag(name = "comment", description = "Comment API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/posts/{postId}/comment")
@@ -19,6 +43,8 @@ public class CommentController {
     private final CommentService COMMENT_SERVICE;
     private final CommentMapper COMMENT_MAPPER;
 
+    @Operation(summary = "Create Comment", description = "해당 게시물에 댓글 생성", responses = {
+            @ApiResponse(responseCode = "201", description = "Create Success", content = @Content(schema = @Schema(implementation = CommentResponseDto.class)))})
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CommentResponseDto createComment(@PathVariable Long postId, @RequestBody CommentCreateRequestDto CommentCreateRequestDto) {
@@ -26,12 +52,16 @@ public class CommentController {
         return COMMENT_MAPPER.getDataFromEntity(createCommentResult);
     }
 
+    @Operation(summary = "Find all Comments", description = "해당 게시물 모든 댓글 가져오기", responses = {
+            @ApiResponse(responseCode = "200", description = "Find Success", content = @Content(schema = @Schema(implementation = CommentResponseDto.class)))})
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Comment> findAllComment() {
         return COMMENT_SERVICE.findAllComment();
     }
 
+    @Operation(summary = "Update Comment", description = "댓글 수정", responses = {
+            @ApiResponse(responseCode = "200", description = "Update Success", content = @Content(schema = @Schema(implementation = CommentResponseDto.class)))})
     @PutMapping
     @ResponseStatus(HttpStatus.OK)
     public CommentResponseDto updateComment(@PathVariable Long postId, @RequestBody CommentUpdateRequestDto commentUpdateRequestDto) {
@@ -39,9 +69,11 @@ public class CommentController {
         return COMMENT_MAPPER.getDataFromEntity(updateCommentResult);
     }
 
+    @Operation(summary = "Delete Comment", description = "해당 id 댓글 삭제", responses = {
+            @ApiResponse(responseCode = "200", description = "Delete Success")})
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Long deleteComment(@PathVariable Long id) {
+    public Long deleteComment(@Parameter(description = "댓글 id", in = ParameterIn.PATH) @PathVariable Long id) {
         COMMENT_SERVICE.deleteComment(id);
         return id;
     }
--- src/main/java/com/techeeresc/tab/domain/member/controller/MemberController.java
+++ src/main/java/com/techeeresc/tab/domain/member/controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.techeeresc.tab.domain.member.controller;
 
-
 import com.techeeresc.tab.domain.member.dto.mapper.MemberMapper;
 import com.techeeresc.tab.domain.member.dto.request.MemberCreateRequestDto;
 import com.techeeresc.tab.domain.member.dto.request.MemberLoginRequestDto;
@@ -8,12 +7,38 @@ import com.techeeresc.tab.domain.member.dto.request.MemberUpdateRequestDto;
 import com.techeeresc.tab.domain.member.dto.response.MemberResponseDto;
 import com.techeeresc.tab.domain.member.entity.Member;
 import com.techeeresc.tab.domain.member.service.MemberService;
+import com.techeeresc.tab.global.exception.response.ErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@ApiResponses(
+        {
+                @ApiResponse(responseCode = "400", description = "BAD REQUEST by Parameter Missing",
+                        content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                ),
+                @ApiResponse(responseCode = "400", description = "BAD REQUEST by Type Mismatch",
+                        content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                ),
+                @ApiResponse(responseCode = "404", description = "NOT FOUND",
+                        content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                ),
+                @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR",
+                        content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                ),
+        }
+)
+@Tag(name = "member", description = "Member API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/member")
@@ -21,7 +46,8 @@ public class MemberController {
     private final MemberService MEMBER_SERVICE;
     private final MemberMapper MEMBER_MAPPER;
 
-    //회원가입하기
+    @Operation(summary = "Create Member", description = "회원가입", responses = {
+            @ApiResponse(responseCode = "201", description = "Create Success", content = @Content(schema = @Schema(implementation = MemberResponseDto.class)))})
     @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
     public MemberResponseDto signupMember(@RequestBody MemberCreateRequestDto MemberCreateRequestDto) {
@@ -29,7 +55,8 @@ public class MemberController {
         return MEMBER_MAPPER.getDataFromEntity(signupMemberResult);
     }
 
-    //로그인
+    @Operation(summary = "Login member", description = "로그인", responses = {
+            @ApiResponse(responseCode = "200", description = "Login Success", content = @Content(schema = @Schema(implementation = MemberResponseDto.class)))})
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
     public MemberResponseDto loginMember(@RequestBody MemberLoginRequestDto MemberLoginRequestDto) {
@@ -41,32 +68,36 @@ public class MemberController {
         }
     }
 
-    //모든 회원 정보 가져오기
+    @Operation(summary = "Find all Members", description = "모든 회원정보 가져오기", responses = {
+            @ApiResponse(responseCode = "200", description = "Find Success", content = @Content(schema = @Schema(implementation = MemberResponseDto.class)))})
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Member> readAllMember() {
         return MEMBER_SERVICE.readAllMember();
     }
 
-    //회원 정보 가져오기
+    @Operation(summary = "Find the Member", description = "해당 id 회원정보 가져오기", responses = {
+            @ApiResponse(responseCode = "200", description = "Find Success", content = @Content(schema = @Schema(implementation = MemberResponseDto.class)))})
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public MemberResponseDto findMember(@PathVariable Long id){
+    public MemberResponseDto findMember(@Parameter(description = "회원 id", in = ParameterIn.PATH) @PathVariable Long id){
         Member member = MEMBER_SERVICE.findMemberById(id);
         return MEMBER_MAPPER.getDataFromEntity(member);
     }
 
-    //회원 정보 수정
+    @Operation(summary = "Update Member", description = "회원정보 수정", responses = {
+            @ApiResponse(responseCode = "200", description = "Update Success", content = @Content(schema = @Schema(implementation = MemberResponseDto.class)))})
     @PutMapping
     public MemberResponseDto updateMember(@RequestBody MemberUpdateRequestDto MemberUpdateRequestDto) {
         Member updateMemberResult = MEMBER_SERVICE.updateMember(MemberUpdateRequestDto);
         return MEMBER_MAPPER.getDataFromEntity(updateMemberResult);
     }
 
-    //회원 삭제
+    @Operation(summary = "Delete Member", description = "해당 id 회원 삭제", responses = {
+            @ApiResponse(responseCode = "200", description = "Delete Success")})
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Long deleteMember(@PathVariable Long id) {
+    public Long deleteMember(@Parameter(description = "회원 id", in = ParameterIn.PATH) @PathVariable Long id) {
         MEMBER_SERVICE.deleteMember(id);
         return id;
     }
