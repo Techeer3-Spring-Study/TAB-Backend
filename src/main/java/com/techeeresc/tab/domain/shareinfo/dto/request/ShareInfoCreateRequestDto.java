5a5781414c5e4ca64cf3e9d4ed40863934cf70bd
Choi Tai <speed5075@gmail.com>
2023-02-08 05:05:00 +0900

    refactor: [#61] Bookmark ShareInfo 수정

--- src/main/java/com/techeeresc/tab/domain/shareinfo/dto/request/ShareInfoCreateRequestDto.java
+++ src/main/java/com/techeeresc/tab/domain/shareinfo/dto/request/ShareInfoCreateRequestDto.java
@@ -10,23 +10,18 @@ import javax.validation.constraints.NotNull;
 @Builder
 public class ShareInfoCreateRequestDto {
 
-  @NotNull
   @Schema(description = "ShareInfo_Title", defaultValue = "Hello")
   private String title;
 
-  @NotNull
   @Schema(description = "ShareInfo_Content", defaultValue = "Wow")
   private String content;
 
-  @NotNull
   @Schema(description = "ShareInfo_Link", defaultValue = "www.google.com")
   private String link;
 
-  @NotNull
   @Schema(description = "ShareInfo_image", defaultValue = "sad.jpg")
   private String image;
 
-  @NotNull
   @Schema(description = "ShareInfo_Hashtag", defaultValue = "#wow")
   private String hashtag;
 }
