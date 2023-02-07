0fea7b106a81752578cf05f74c22de00676207e0
Choi Tai <speed5075@gmail.com>
2023-02-08 05:11:03 +0900

    feat: [#61] Bookmark ShareInfo UpdateDTO Swagger추가

--- src/main/java/com/techeeresc/tab/domain/shareinfo/dto/request/ShareInfoUpdateRequestDto.java
+++ src/main/java/com/techeeresc/tab/domain/shareinfo/dto/request/ShareInfoUpdateRequestDto.java
@@ -1,15 +1,31 @@
 package com.techeeresc.tab.domain.shareinfo.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
+
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Builder
 public class ShareInfoUpdateRequestDto {
+
+  @NotNull
+  @Schema(description = "ShareInfo_id", defaultValue = "11")
   private Long id;
+
+  @Schema(description = "ShareInfo_title", defaultValue = "LoL")
   private String title;
+
+  @Schema(description = "ShareInfo_content", defaultValue = "WOW")
   private String content;
+
+  @Schema(description = "ShareInfo_link", defaultValue = "www.naver.com")
   private String link;
+
+  @Schema(description = "ShareInfo_image", defaultValue = "hello.jpg")
   private String image;
+
+  @Schema(description = "ShareInfo_hashtag", defaultValue = "#wow")
   private String hashtag;
 }
